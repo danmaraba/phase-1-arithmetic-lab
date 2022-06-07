f4b15c7b6c3949be5ec29d9c54d3db918384498d
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-06-07 05:46:02 +0300

    commit tree 54c679eedf6fb26891c403ea0b4c48123d2c0487

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1=2;
+const num2=31;
+const multiply=num1*num2;
+
+ const random=Math.floor(Math.random() *10) +2;
+
+ const num3=16;
+ const num4=12;
+ const mod=num3 % num4
+
+ const max= Math.max(7,9,11,13,15,17,20)
